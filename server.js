26fce65ab9d2794a39c900d9d0a11e7072c1d875
Roman Slastion` <garik@example.com>
2025-05-21 14:29:08 +0200

    Убрал frontend из индекса, добавил обновление backend

--- server.js
+++ server.js
@@ -1,27 +1,67 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+app.post('/webhook', async (req, res) => {
+  const message = req.body.message;
+
+  if (message && message.text === '/start') {
+    const chatId = message.chat.id;
+
+    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      chat_id: chatId,
+      parse_mode: "Markdown",
+      disable_web_page_preview: true,
+      text: `*Добро пожаловать в Web3/Crypto University!*
+
+Ты оказался на территории свободного мышления, анархии и антихрупкого образования.
+
+Здесь мы не просим разрешения у банков. Мы учимся, как обходить систему, хранить свободу и приумножать активы.
+
+---
+
+*Что нужно для старта?*
+
+1. **Кошелёк и контроль над своими ключами**
+2. **Биржа для пополнения** — выбери и зарегистрируйся:
+
+- [Binance](https://accounts.binance.com/register?ref=477681384)
+- [Bybit](https://www.bybit.com/invite?ref=B1XGLW)
+- [Evedex]https://invite.evedex.com/ixjrpwo4)
+- [Bingx](https://bingx.com/invite/4KQ2KY/)
+
+3. **Желание стать независимым**.
+
+---
+
+Нажми кнопку ниже и начни обучение прямо в Telegram:
+`,
+      reply_markup: {
+        inline_keyboard: [[
+          {
+            text: "🚀 Начать обучение",
+            web_app: {
+              url: "https://t.me/Web3CryptoUniversity_bot?startapp"
+            }
+          }
+        ]]
+      }
+    });
+
+    return res.sendStatus(200);
+  }
+
+  res.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Web3 University Backend is running!");
-});
-
-app.get("/status", (req, res) => {
-  res.json({
-    success: true,
-    message: "Backend is alive",
-    timestamp: new Date().toISOString()
-  });
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
